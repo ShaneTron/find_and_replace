3111db0f26448c2df5fc6487d44c901f6025f21b
Qianqian Hu and Shane Robinson <qianqian@>
2017-11-15 16:26:49 -0800

    add the weird ass word thing.

--- lib/find_and_replace.rb
+++ lib/find_and_replace.rb
@@ -2,8 +2,8 @@ def find_and_replace(sentence, word1, word2)
   new_array = sentence.split(/ /)
   new_string = ""
   new_array.each do |word|
-    if word == word1
-    word = word2
+    if word.include? word1
+    word.gsub!(word1, word2)
     new_string.concat(word).concat(" ")
     else
     new_string.concat(word).concat(" ")
--- spec/find_and_replace_spec.rb
+++ spec/find_and_replace_spec.rb
@@ -6,4 +6,8 @@ describe('find_and_replace') do
   it("finds and replaces a word in a string") do
   expect(find_and_replace("Hello world", "world", "universe")).to(eq("Hello universe"))
   end
+  it("takes into account partial matches.") do
+  expect(find_and_replace("I am walking my cat to the cathedral", "cat", "dog")).to(eq("I am walking my dog to the doghedral"))
+  end
+
 end
